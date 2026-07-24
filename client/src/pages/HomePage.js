686bd324515b43bec0b7a670a5ad28e7509c7a73
Raghav0100 <gupta.raghav091102@gmail.com>
2026-07-24 18:35:33 +0530

    Image Fi

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -171,10 +171,13 @@ const HomePage = () => {
                 style={{ width: "18rem" }}
               >
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
-                  className="card-img-top"
-                  alt={p.name}
-                />
+  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+  className="card-img-top"
+  alt={p.name}
+  onError={(e) => {
+    console.log("Image failed:", e.target.src);
+  }}
+/>
 
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
